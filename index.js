cb1717ecee7e17873fb9655b044f4bd18b8f3ba9
“Damir <d.magdeev@dodopizza.com>
2021-03-18 14:24:16 +0300

    v1 release

--- index.js
+++ index.js
@@ -5,32 +5,46 @@ const github = require('@actions/github');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-    
-    const myToken = core.getInput('myToken');
-    const octokit = github.getOctokit(myToken)
+    try {
+        const ms = core.getInput('milliseconds');
+        core.info(`Waiting ${ms} milliseconds ...`);
 
-    const context = github.context;
+        const myToken = core.getInput('myToken');
+        const octokit = github.getOctokit(myToken)
 
-    const newIssue = await octokit.issues.create({
-      ...context.repo,
-      title: 'New issue!',
-      body: 'Hello Universe!'
-    });
-    
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+        const context = github.context;
+        context.repo.owner
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+        const {repository} = await octokit.graphql(
+            `
+    {
+      repository(owner: ${context.repo.owner}, name: ${context.repo.repo}) {
+        issues(last: 3) {
+          edges {
+            node {
+              title
+            }
+          }
+        }
+      }
+    }
+  `,
+            {
+                headers: {
+                    authorization: `token secret123`,
+                },
+            }
+        );
+        console.log(repository);
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+        await wait(parseInt(ms));
+        core.info((new Date()).toTimeString());
+
+        core.setOutput('time', new Date().toTimeString());
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
