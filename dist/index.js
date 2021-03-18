f4b7f3fdfb89b64cb235fb1b304999f1ee7869e8
“Damir <d.magdeev@dodopizza.com>
2021-03-18 14:37:26 +0300

    v1 release

--- dist/index.js
+++ dist/index.js
@@ -12,32 +12,36 @@ const github = __nccwpck_require__(438);
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-    
-    const myToken = core.getInput('myToken');
-    const octokit = github.getOctokit(myToken)
+    try {
+        const ms = core.getInput('milliseconds');
+        core.info(`Waiting ${ms} milliseconds ...`);
 
-    const context = github.context;
+        const myToken = core.getInput('myToken');
+        const octokit = github.getOctokit(myToken)
+        const context = github.context;
 
-    const newIssue = await octokit.issues.create({
-      ...context.repo,
-      title: 'New issue!',
-      body: 'Hello Universe!'
-    });
-    
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    //     const {repository} = await octokit.graphql(`
+    // {
+    //   repository(owner: "${context.repo.owner}", name: "${context.repo.repo}") {
+    //     issues(last: 3) {
+    //       edges {
+    //         node {
+    //           title
+    //         }
+    //       }
+    //     }
+    //   }
+    // }`);
+    //     console.log(repository);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+        core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+        await wait(parseInt(ms));
+        core.info((new Date()).toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        core.setOutput('time', new Date().toTimeString());
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
